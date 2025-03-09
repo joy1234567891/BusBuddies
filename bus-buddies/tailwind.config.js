7ccccae5d0f9a65e19b7c0a77ecf855e6a6292e7
Katja Radovic-Jonsson <krj_mc@outlook.com>
2025-03-08 20:58:27 -0800

    font

--- bus-buddies/tailwind.config.js
+++ bus-buddies/tailwind.config.js
@@ -1,11 +1,10 @@
 module.exports = {
     content: ["./src/**/*.{js,ts,jsx,tsx}"],
     theme: {
-      extend: {
-        colors: {
+        extend: {
+            colors: {
+            },
         },
-      },
     },
     plugins: [],
-  };
-  
+};
